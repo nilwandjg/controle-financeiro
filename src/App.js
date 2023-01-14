34589e3dca4bde2b04ee5f4602cb92b104568e4c
nilwandjg <nilwandjg@yahoo.com.br>
2023-01-14 04:50:18 -0300

    Criação do componente Form. Mais alguns detalhes dos componentes Resume e ResumeItem.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import GlobalStyle from './styles/global'
 import Header from './components/Header'
 import Resume from './components/Resume'
+import Form from './components/Form'
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
         <>
             <Header />
             <Resume />
+            <Form />
             <GlobalStyle />
         </>
     )
--- src/components/ResumeItem/index.js
+++ src/components/ResumeItem/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import * as C from "./styles"
 
-const ResumeItem = () => {
+const ResumeItem = ({ title, Icon, value }) => {
     return (
         <C.Container>
             <C.Header>
-                <C.HeaderTitle>teste</C.HeaderTitle>
+                <C.HeaderTitle>{title}</C.HeaderTitle>
+                <Icon />
             </C.Header>
-            <C.Total>1000</C.Total>
+            <C.Total>{value}</C.Total>
         </C.Container>
     )
 }
 
 export default ResumeItem
+
